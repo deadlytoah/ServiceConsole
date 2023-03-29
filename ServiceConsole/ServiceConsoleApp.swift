25d26e076bf90c1b93b6a02dc538f0246179146b
Hee Suk Shin <heesuk@gmail.com>
2023-03-29 21:37:26 +0900

    Initial Commit

---
+++ ServiceConsole/ServiceConsoleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceConsoleApp.swift
+//  ServiceConsole
+//
+//  Created by Hee Suk Shin on 2023/03/29.
+//
+
+import SwiftUI
+
+@main
+struct ServiceConsoleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
