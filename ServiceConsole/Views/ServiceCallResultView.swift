9ffa2ce5b5dc194d90cbbfe59df3166ec4b459f0
Hee Suk Shin <heesuk@gmail.com>
2023-05-10 15:56:09 +0900

    Include license in the source code.

--- ServiceConsole/Views/ServiceCallResultView.swift
+++ ServiceConsole/Views/ServiceCallResultView.swift
@@ -1,3 +1,21 @@
+/*
+ * GUI Console for IPC Services
+ * Copyright (C) 2023  Hee Shin
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 //
 //  ServiceCallResultView.swift
 //  ServiceConsole
@@ -26,7 +44,7 @@ struct ServiceCallResultView: View {
     let service: Service
     let command: Command
     let arguments: [String]
-    
+
     var body: some View {
         VStack(alignment: .leading) {
             if let response = self.response {
